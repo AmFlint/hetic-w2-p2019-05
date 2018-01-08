17cf1a26fb4abcf62c82653da6bf2daf31588287
AmFlint <amasselot@hotmail.com>
2018-01-09 00:01:35 +0100

    remove not needed scripts and pages files

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-import { User } from './user';
-
-console.log('Welcome !');
-
-let name = 'Bastien';
-const user = new User(name);
--- src/js/user.js
+++
@@ -1,5 +0,0 @@
-export class User {
-  constructor(name) {
-    console.log(name + ' user created');
-  }
-}
